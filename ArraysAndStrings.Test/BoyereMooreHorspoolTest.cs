ee5dd044732718628cf14ffb9a54505fc28628d5
Grigory Bushuev <grigorybushuev@hotmail.com>
2017-11-05 22:11:30 +0300

    Add BoyereMooreHorspool substring search algorithm

---
+++ ArraysAndStrings.Test/BoyereMooreHorspoolTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace ArraysAndStrings.Test
+{
+    [TestFixture]
+    public class BoyereMooreHorspoolTest
+    {
+
+#region General cases
+        [Test]
+        [TestCase("There is the truth in that sentence, the real truth", "truth", 13, 46)]
+        public void BoyereMooreHorsPoool_OnPredefinedParameters_ReturnsExpectedIndexes(string toSearch, string toFind, params int[] expectedIndexes)
+        {
+            //Act
+            var result = BoyereMooreHorspool.GetMatches(toSearch, toFind);
+            //Assert
+            Assert.IsInstanceOf<IEnumerable<int>>(result);
+            Assert.That(result.ToList(), Is.EqualTo(expectedIndexes));
+        }
+
+        [Test]
+        [TestCase("There is the truth in that sentence, the real truth", "untruth")]
+        public void BoyereMooreHorsPoool_OnPredefinedParameters_ReturnsEmptyCollection(string toSearch, string toFind)
+        {
+            //Act
+            var result = BoyereMooreHorspool.GetMatches(toSearch, toFind);
+            //Assert
+            Assert.IsInstanceOf<IEnumerable<int>>(result);
+            Assert.That(result.ToList(), Is.Empty);
+        }
+
+        #endregion
+
+        #region Edge cases
+        [Test]
+        public void BoyereMooreHorsPoool_OnEmptyToSearchString_ReturnsArgumentOutOfRangeException()
+        {
+            //Assert
+            Assert.That(() => BoyereMooreHorspool.GetMatches(null, "ToSearch").ToArray(), Throws.TypeOf<ArgumentOutOfRangeException>().With.Property("ParamName").EqualTo("toSearch"));
+        }
+
+        [Test]
+        public void BoyereMooreHorsPoool_OnEmptyToFindString_ReturnsArgumentOutOfRangeException()
+        {
+            //Assert
+            Assert.That(() => BoyereMooreHorspool.GetMatches("toSearch", null).ToArray(), Throws.TypeOf<ArgumentOutOfRangeException>().With.Property("ParamName").EqualTo("toFind"));
+            Assert.Throws<ArgumentOutOfRangeException>(() => BoyereMooreHorspool.GetMatches("toSearch", null).ToArray());
+        }
+        #endregion
+
+
+
+
+    }
+}
---
+++ ArraysAndStrings/BoyereMooreHorspool.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysAndStrings
+{
+    public static class BoyereMooreHorspool
+    {
+        private static IDictionary<char, int> GetBadMatches(string pattern)
+        {
+            var badMatches = new Dictionary<char, int>();
+            var patternLength = pattern.Length;
+            for (var i = 0; i < patternLength; i++)
+            {
+                badMatches[pattern[i]] = patternLength - i - 1;
+            }
+            return badMatches;
+        }
+
+        public static IEnumerable<int> GetMatches(string toSearch, string toFind)
+        {
+            if (string.IsNullOrEmpty(toSearch))
+                throw new ArgumentOutOfRangeException(nameof(toSearch));
+
+            if (string.IsNullOrEmpty(toFind))
+                throw new ArgumentOutOfRangeException(nameof(toFind));
+
+            var badMatches = GetBadMatches(toFind);
+            var toFindLength = toFind.Length;
+            for (var i = toFindLength; i < toSearch.Length; i++)
+            {
+                var currentChar = toSearch[i];
+                if (!badMatches.ContainsKey(currentChar))
+                {
+                    i += toFindLength - 1;
+                }
+                else
+                {
+                    i += badMatches[currentChar];
+                    var j = i;
+                    var findIndex = toFindLength - 1;
+                    while (findIndex > 0 && String.Equals(toSearch[j], toFind[findIndex]))
+                    {
+                        j--;
+                        findIndex--;
+                    }
+                    if (findIndex == 0)
+                        yield return j;
+                }
+            }
+        }
+    }
+}
