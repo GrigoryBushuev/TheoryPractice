b18ab6bd876a1b8c2a7a34c78ab8c2c88a9fc1ef
Grigory Bushuev <grigorybushuev@hotmail.com>
2018-04-18 17:18:02 +0300

    Add Fibonacci

---
+++ DynamicProgramming.Tests/FibonacciTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace DynamicProgramming.Tests
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 5)]
+        [TestCase(5, 8)]
+        [TestCase(6, 13)]
+        [TestCase(7, 21)]
+        [TestCase(8, 34)]
+        [TestCase(9, 55)]
+        public void GetNth_OnValidParam_ReturnsExpextedValue(int n, int expectedValue)
+        {
+            //Act
+            var actualResult = Fibonacci.GetNth(n);
+            //Assert
+            Assert.AreEqual(expectedValue, actualResult);
+        }
+
+        [TestCase(0, new int[]{ 1 })]
+        [TestCase(1, new int[] { 1, 1 })]
+        [TestCase(2, new int[] { 1, 1, 2 })]
+        [TestCase(9, new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 })]
+        public void GetFibonacciSequence_OnValidParam_ReturnsExpextedValue(int n, int[] expectedValue)
+        {
+            //Act
+            var actualResult = Fibonacci.GetFibonacciSequence(n);
+            //Assert
+            Assert.AreEqual(expectedValue, actualResult);
+        }
+    }
+}
---
+++ DynamicProgramming/Fibonacci.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicProgramming
+{
+    public static class Fibonacci
+    {
+        public static int GetNth(int n)
+        {
+            if (n < 2)
+                return 1;
+            var a = 1;
+            var b = 1;
+            var c = 2;
+
+            for (var i = 2; i < n; i++) {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return c;
+        }
+
+        public static IEnumerable<int> GetFibonacciSequence(int n)
+        {
+            yield return 1;
+            if (n == 0)
+                yield break;
+                     
+            var a = 1;
+            var b = 1;
+            var c = 2;
+
+            yield return 1;
+            if (n == 1)
+                yield break;
+
+            yield return c;
+
+            for (var i = 2; i < n; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+                yield return c;
+            }
+        }
+
+    }
+}
