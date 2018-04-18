98108a7760ee0cafd2411d782503d84cc1ba8645
Grigory Bushuev <grigorybushuev@hotmail.com>
2018-04-18 21:54:29 +0300

    Add StringReverse

---
+++ ArraysAndStrings.Test/StringUtilsTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ArraysAndStrings.Test
+{
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        [TestCase("ABC", "CBA")]
+        [TestCase("ABCD", "DCBA")]
+        public void Reverse_OnValidParam_ReturnsExpectedResult(string param, string expectedResult)
+        {
+            //Act
+            var actualResult = param.Reverse();
+            //Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        public void Reverse_OnEmptyParam_ThrowsArgumentNullException(string param, string expectedResult)
+        {
+            //Assert
+            Assert.Catch<ArgumentNullException>(() => param.Reverse());
+        }
+
+    }
+}
---
+++ ArraysAndStrings/StringUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysAndStrings
+{
+    public static class StringUtils
+    {
+        public static string Reverse(this string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            var sb = new StringBuilder(text);
+            for (var i = 0; i < sb.Length / 2; i++) {
+                var temp = sb[i];
+                sb[i] = sb[sb.Length - 1 - i];
+                sb[sb.Length - 1 - i] = temp;
+            }
+            return sb.ToString();
+        }
+    }
+}
