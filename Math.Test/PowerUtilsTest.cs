e6dc33dec5f3e6cb1cb242b15cfe1a43b6f3ed8c
Grigory Bushuev <grigorybushuev@hotmail.com>
2018-04-27 12:56:50 +0300

    PowerOfQ

---
+++ Math.Test/PowerUtilsTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Math.Test
+{
+    [TestFixture]
+    public class PowerUtilsTest
+    {
+        [TestCase(2, 3u, 8)]
+        [TestCase(3, 0u, 1)]
+        [TestCase(0, 1u, 0)]
+        [TestCase(0, 0u, 1)]
+        public void PowerOfQ_ReturnsExpectedResult(int p, uint q, int expectedResult)
+        {
+            var actualResult = PowerUtils.PowerOfQ(p, q);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+    }
+}
---
+++ Math/Powerutils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public static class PowerUtils
+    {
+        /// <summary>
+        /// The function takes as input integers P and Q and returns P to
+        /// the power of Q. Note any assumptions you make and the complexity of the
+        /// algorithm. We expect you to do better than O(Q).
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public static int PowerOfQ(int p, uint q)
+        {
+            if (q == 0)
+                return 1;
+
+            var result = PowerOfQ(p, q >> 1);
+            var product = result * result;
+            return (q % 2 == 0) ? product : p * product;
+        }
+
+
+    }
+}
